c99b1dd07513c8d17d2a84dad3609124c517faf2
Himaanshusingh12 <himaanshu1601@gmail.com>
2024-04-07 12:46:14 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './Accordion';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion
+        title="What is your return policy?"
+        content="This is the content for Accordion Item 1.This is the content for Accordion Item 1.This is the content for Accordion Item 1.This is the content for Accordion Item 1.This is the content for Accordion Item 1.This is the content for Accordion Item 1.vThis is the content for Accordion Item 1.This is the content for Accordion Item 1.This is the content for Accordion Item 1."
+      />
+      <Accordion
+        title="How do I track my order?"
+        content="This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2."
+      />
+      <Accordion
+        title="Can i purchase items again?"
+        content="This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2.This is the content for Accordion Item 2."
+      />
     </div>
   );
-}
+};
 
 export default App;
